22e3726e9d0c98bc50f58dabe734bc1a7b7a67de
Michael Palarya <19212750+MPalarya@users.noreply.github.com>
2023-04-12 22:37:22 +0300

    improve the table UI + preperation for better manual edits

--- src/components/charts/tableChart.vue
+++ src/components/charts/tableChart.vue
@@ -1,49 +1,39 @@
 <template>
-  <div style="height: 100%;">
+  <div style="height: 100%">
     <v-data-table
-      v-show="this.data && this.data.length"
+      id="mainTable"
+      style="height: 100%; overflow: auto !important;"
+      v-if="this.data && this.data.length"
       :headers="headers"
       :items="data"
-      :sort-by="['name']"
-      :sort-desc="[false, true]"
-      item-key="name"
+      :sort-by="'value'"
+      sort-desc
+      must-sort
+      item-key="symbol"
       hide-default-footer
       disable-pagination
       class="transparent"
       fixed-header
-      style="height: 100%"
       show-expand
-      @click:row="(item, slot) => slot.expand(!slot.isExpanded)"
       :expanded.sync="expanded"
+      @click:row="(item, slot) => slot.expand(!slot.isExpanded)"
     >
-      <template #[`header.actions`]="{ header }">
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on, attrs }">
-            <v-icon
-              v-on="on"
-              v-bind="attrs"
-              small
-              @click="showManualStockDialog"
-              >mdi-plus-circle</v-icon
-            >{{ header.text }}
-          </template>
-          <span>Add Stock Manually</span>
-        </v-tooltip>
-      </template>
+      <!--ADD THIS ABOVE IF YOU WANT TO TRY RESIZABLE COLUMNS-->
+      <!--v-resizable-columns-->
 
       <template v-slot:expanded-item="{ item: ParentItem }">
-        <td :colspan="headers.length" style="padding: 0px">
+        <td :colspan="headers.length" style="padding: 0">
           <v-data-table
             id="nestedTable"
             :headers="nestedHeaders"
             :items="ParentItem.transactions"
-            :sort-by="['name']"
-            :sort-desc="[false, true]"
+            :sort-by="'date_at_purchase'"
+            must-sort
             item-key="name"
             hide-default-footer
             disable-pagination
             class="alternativeBackground"
-            fixed-header
+            dense
           >
             <template #[`item.date_at_purchase`]="{ item }">
               {{ item.date_at_purchase }}
@@ -69,14 +59,54 @@
                     .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
               }}
             </template>
+            <template #[`item.actions`]>
+              <v-btn
+                text
+                icon
+                @click.native.stop
+                @click="notImplementedAlertVisible ^= true"
+                class="invisibleButton"
+              >
+                <v-icon>mdi-trash-can-outline</v-icon>
+              </v-btn>
+            </template>
           </v-data-table>
         </td>
       </template>
 
-      <template #[`item.name`]="{ item }">
-        <v-btn block text :href="item.url" target="_blank">
-          {{ item.name }}
+      <template #[`header.data-table-expand`]>
+        <v-btn text icon>
+          <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon
+              v-on="on"
+              v-bind="attrs"
+              @click="showManualStockDialog"
+            >
+              mdi-plus-circle-outline
+            </v-icon>
+          </template>
+          <span>Add Investment</span>
+        </v-tooltip>
         </v-btn>
+      </template>
+
+      <template #[`item.name`]="{ item }">
+        <div class="truncate">
+          <v-btn
+            outlined
+            :rounded="!/[a-z]/i.test(item.symbol)"
+            width="80"
+            small
+            :href="item.url"
+            target="_blank"
+            style="margin-right: 15px"
+          >
+            {{ item.symbol }}
+          </v-btn>
+
+          <span>{{ item.name }}</span>
+        </div>
       </template>
 
       <template #[`item.units`]="{ item }">
@@ -109,34 +139,82 @@
       </template>
 
       <template #[`item.net_change`]="{ item }">
-        {{
-          "₪" +
-            item.net_change
+        <v-chip
+          ripple
+          label
+          style="width: 100px;"
+          class="justify-center"
+          color="rgba(255, 255, 255, 0.1)"
+          :text-color="
+            item.net_change > 0
+              ? 'success'
+              : item.net_change < 0
+              ? 'error'
+              : undefined
+          "
+        >
+          {{
+          (item.net_change &lt; 0 ? "-" : "") + "₪" +
+            Math.abs(item.net_change)
               .toFixed()
               .toString()
               .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-        }}
+          }}
+        </v-chip>
       </template>
 
       <template #[`item.percent_change`]="{ item }">
-        {{
-          item.percent_change
-            .toFixed(2)
-            .toString()
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "%"
-        }}
+        <v-chip
+          ripple
+          label
+          style="width: 75px;"
+          class="justify-center"
+          color="rgba(255, 255, 255, 0.1)"
+          :text-color="
+            item.net_change > 0
+              ? 'success'
+              : item.net_change < 0
+              ? 'error'
+              : undefined
+          "
+        >
+          {{
+            item.percent_change
+              .toFixed(2)
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "%"
+          }}
+        </v-chip>
       </template>
 
       <template #[`item.chart`]="{ item }">
         <inner-table-chart :data-array="instOverTime(item.inst_over_time)" />
       </template>
+
+      <template #[`item.actions`]>
+        <div class="truncate">
+          <v-btn text icon @click.native.stop>
+            <v-icon>mdi-plus-circle-outline</v-icon>
+          </v-btn>
+          <v-btn
+            text
+            icon
+            @click.native.stop
+            @click="notImplementedAlertVisible ^= true"
+          >
+            <v-icon>mdi-trash-can-outline</v-icon>
+          </v-btn>
+        </div>
+      </template>
+
     </v-data-table>
-    <v-skeleton-loader
-      v-show="!this.data || !this.data.length"
-      max-height="100%"
-      type="table-thead, table-tbody, table-tfoot"
-    >
+
+    <v-skeleton-loader v-else type="table-thead, table-tbody">
     </v-skeleton-loader>
+
+    <v-snackbar v-model="notImplementedAlertVisible">
+      Sorry, Not Implemented Yet!
+    </v-snackbar>
   </div>
 </template>
 
@@ -164,43 +242,28 @@ export default {
     let daysBack = 7;
 
     return {
+      notImplementedAlertVisible: false,
       daysBack: daysBack,
       dialog: false,
       expanded: [],
       nestedHeaders: [
-        {
-          text: "Date of Purchase",
-          value: "date_at_purchase",
-          align: "center",
-        },
-        {
-          text: "price at Purchase",
-          value: "price_at_purchase",
-          align: "center",
-        },
-        { text: "Units", value: "units", align: "center" },
-        { text: "Cost", value: "cost", align: "center" },
+        { text: "", value: "", sortable: false },
+        { text: "Purchase Date", value: "date_at_purchase" },
+        { text: "Purchase Price", value: "price_at_purchase" },
+        { text: "Quantity", value: "units" },
+        { text: "Value", value: "cost" },
+        { text: "", value: "actions", sortable: false, align: "right" },
       ],
       headers: [
-        {
-          text: "",
-          value: "actions",
-          sortable: false,
-          width: "10px",
-        },
-        {
-          text: "Instrument",
-          align: "center",
-          value: "name",
-        },
-        { text: "Symbol", value: "symbol" },
-        { text: "Units", value: "units" },
-        { text: "Price", value: "price" },
-        { text: "Value (₪)", value: "value" },
-        { text: "Return (₪)", value: "net_change" },
-        { text: "Return (%)", value: "percent_change" },
+        { text: "", value: "data-table-expand", align: 'center' },
+        { text: "Name", value: "name" }, // width is set by `.truncate` CSS rule
+        { text: "Quantity", value: "units", width: "105" },
+        { text: "Price", value: "price", minWidth: "150" },
+        { text: "Value", value: "value", minWidth: "150" },
+        { text: "Gain", value: "net_change", minWidth: "135" },
+        { text: "Gain (%)", value: "percent_change", minWidth: "115" },
         { text: "Last " + daysBack + " days", value: "chart", sortable: false },
-        { text: "", value: "data-table-expand" },
+        { text: "", value: "actions", sortable: false, minWidth: "150", align: "right" },
       ],
     };
   },
@@ -216,29 +279,61 @@ export default {
 </script>
 
 <style lang="scss">
-tbody {
-  tr:hover {
-    background-color: transparent !important;
-  }
+.invisibleButton {
+  opacity: 0 !important;
 }
+
+.invisibleButton:hover {
+  opacity: 1 !important;
+}
+
+.truncate {
+  max-width: 25vw;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+/*.resizable-columns-divider div {*/
+/*  background-color: white !important;*/
+/*  box-shadow: #ef968e;*/
+/*}*/
+
+tbody > tr:hover {
+  background-color: rgba(255, 255, 255, 0.04) !important;
+}
+
 th {
-  font-size: 20px !important;
+  font-size: 14px !important;
+  background-color: var(--v-background-base) !important;
 }
-td {
-  font-size: 16px !important;
+
+#mainTable td {
   font-weight: bold;
+  cursor: pointer;
 }
+
+#nestedTable td {
+  cursor: default;
+}
+
 .v-data-table__wrapper {
   height: 100%;
 }
+::-webkit-scrollbar {
+  width: 13px !important;
+  height: 13px !important;
+}
+::-webkit-scrollbar-corner {
+  background: transparent;
+}
 
-.v-data-table .v-data-table-header tr th {
-  background-color: var(--v-background-base) !important;
-}
-.v-skeleton-loader__table-thead {
-  background-color: transparent !important;
-}
+
+.v-data-table,
+.v-data-table__wrapper,
+.v-skeleton-loader__table-thead,
 .v-skeleton-loader__table-tbody {
   background-color: transparent !important;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
+import ResizableColumns from "@spacebnd/vuetify-data-table-resizable-columns";
 import App from "@/app.vue";
 import "@/registerServiceWorker";
 import router from "@/router";
 import vuetify from "@/plugins/vuetify.js";
 import { auth } from "@/firebase.js";
 
+Vue.use(ResizableColumns);
 Vue.config.productionTip = false;
 let app;
 auth.onAuthStateChanged(() => {
